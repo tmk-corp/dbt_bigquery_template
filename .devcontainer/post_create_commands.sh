378f29ef95899cdd1168e3b70f35da213a166a80
Shun Asai <syun.asai@gmail.com>
2023-03-09 08:08:43 +0000

    Add direnv

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2016
+
+project_dir="$(cd "$(git rev-parse --show-toplevel)" && pwd -P)"
 
 sudo chown vscode:vscode /home/vscode/.config
+
+# Enable direnv
+echo 'eval "$(direnv hook bash)"' >> /home/vscode/.bashrc
+direnv allow "${project_dir}"
